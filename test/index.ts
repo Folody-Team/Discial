417c40c1eb67f8afc5f45c8f46f1e682e38e45c5
Từ Thắng Phát <81029660+phatdev12@users.noreply.github.com>
2022-11-03 23:14:40 +0700

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@
 import { Client } from '../packages';
 
 const client = new Client({
-	token: 'MTAyMjUzMDkyMDcwOTg4MTkyNg.GdEWHz.2n6j4CQN56o9LvmDlKvYx3wR3nwJwcfZCU8tvY'
+	token: process.env.TOKEN
 });
 
 
