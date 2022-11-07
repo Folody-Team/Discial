c04fc5b88b2523bbea0e4dc6dc750c40bbe19757
phatdev <tuphat3456tt@gmail.com>
2022-11-07 21:51:12 +0700

    Remove token

--- lib/discial-api/test/index.test.ts
+++ lib/discial-api/test/index.test.ts
@@ -2,7 +2,7 @@ import {Channel} from '../src/index'
 
 
 const channel = Channel
-channel.init('MTAyMjUzMDkyMDcwOTg4MTkyNg.GeLQjS.Etaqhy4_zZ4Lrdw-CmpI0PiSXVMWm97euWiy0k')
-channel.getChannel('955639718815621151').then((data) => {
+channel.init('TOKEN HERE')
+channel.getChannel('ID CHANNEL').then((data) => {
   console.log(data)
 })
